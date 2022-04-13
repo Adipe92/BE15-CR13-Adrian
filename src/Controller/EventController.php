9a8c29350b98dea11ab04f0c7f89947b20b4c1f1
Adrian Pedziwiatr <adrianpe092@gmail.com>
2022-04-13 12:25:05 +0200

    project and files ended

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -15,10 +15,12 @@ use App\Form\EventType;
 class EventController extends AbstractController
 {
     #[Route('/', name: 'event')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $events = $doctrine->getManager()->getRepository(Events::class)->findAll();
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            // 'controller_name' => 'EventController',
+            "events" => $events
         ]);
     }
 
@@ -52,4 +54,59 @@ class EventController extends AbstractController
 /* now to make the form we will add this line form->createView() and now you can see the form in create.html.twig file  */
        return $this->render('event/create.html.twig', ['form' => $form->createView()]);
    }
+
+   #[Route('/details/{id}', name: 'details_event')]
+    public function detalsEvent($id, ManagerRegistry $doctrine): Response
+    {
+        $event = $doctrine->getManager()->getRepository(Events::class)->find($id);
+        return $this->render('event/details.html.twig', [
+            // 'controller_name' => 'EventController',
+            "event" => $event
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit_event')]
+    public function editEvent($id, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $event = $doctrine->getManager()->getRepository(Events::class)->find($id);
+        $form = $this->createForm(EventType::class, $event);
+ 
+        $form->handleRequest($request);
+ 
+ /* Here we have an if statement, if we click submit and if  the form is valid we will take the values from the form and we will save them in the new variables */
+        if ($form->isSubmitted() && $form->isValid()) {
+            $now = new \DateTime('now');
+ 
+  // taking the data from the inputs with the getData() function and assign it to the $todo variable
+            $event = $form->getData();
+            $event->setEventDate($now);  // this field is not included in the form so we set the today date
+            $em = $doctrine->getManager();
+            $em->persist($event);
+            $em->flush();
+ 
+            $this->addFlash(
+                'notice',
+                'Event Edited'
+                );
+      
+            return $this->redirectToRoute('event');
+        }
+ 
+ /* now to make the form we will add this line form->createView() and now you can see the form in create.html.twig file  */
+        return $this->render('event/edit.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/delete/{id}', name: 'delete_event')]
+    public function deleteEvent($id, ManagerRegistry $doctrine): Response
+    {
+        $event = $doctrine->getManager()->getRepository(Events::class)->find($id);
+        $em = $doctrine->getManager();
+        $em->remove($event);
+
+        $em->flush();
+        $this->addFlash("notice", "Event Removed!");
+        return $this->redirectToRoute("event"); 
+    }
+
+
 }
--- templates/db/event_company.sql
+++ templates/db/event_company.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 09 Kwi 2022, 17:59
+-- Czas generowania: 13 Kwi 2022, 12:22
 -- Wersja serwera: 10.4.22-MariaDB
 -- Wersja PHP: 8.1.2
 
@@ -64,6 +64,21 @@ CREATE TABLE `events` (
   `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Zrzut danych tabeli `events`
+--
+
+INSERT INTO `events` (`id`, `event_name`, `event_date`, `description`, `picture`, `capacity`, `email`, `address`, `phone_nb`, `url`, `type`, `zip`, `city`) VALUES
+(2, 'Opener Festival', '2022-04-12 07:38:28', 'The bigest music Festival in the East Europe. Our Bands and artists: Dua Lipa, Maneskin, The Chemical Brothers, The smile, Royal Blood, Years&Years, Desroyer, Flohio, Iceage, Inhaler. Opener Festival it\'s a big 3 days festival with the live music.', 'https://cdn.pixabay.com/photo/2016/11/23/15/48/audience-1853662__340.jpg', 10000, 'openerfestival@mail.com', 'Airport Gdynia Poland', 254213232, 'www.opener.pl', 'Music', '220014', 'Gdynia'),
+(3, 'Music and food', '2022-04-12 07:51:10', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2018/10/09/19/56/music-3735836__340.jpg', 10000, 'mfood@mail.com', 'Bug Street 15', 2147483647, 'londonfood.com', 'Music', '12487', 'London'),
+(4, 'Swan Lake', '2022-04-12 07:57:54', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2016/11/29/09/24/swan-1868697__340.jpg', 2000, 'swanlake@royaloper.com', 'Opera Street 20', 231478569, 'royalopera.com', 'Theather', '17855', 'London'),
+(5, 'Open Dance Festival', '2022-04-12 08:12:28', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2017/11/15/13/53/hands-2952054__340.jpg', 2000, 'dance@festival.com', 'Sun Street 15', 124455889, 'www.dancedance.com', 'Music', '124877', 'Liverpool'),
+(6, 'Movie Festival', '2022-04-12 08:15:20', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2017/07/13/23/11/cinema-2502213__340.jpg', 1000, 'moviefestival.com', 'Main Street 20', 879544103, 'movie.com', 'Theather', '7854412', 'London'),
+(7, 'Football England - Poland', '2022-04-12 08:26:38', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2014/10/14/20/24/soccer-488700__340.jpg', 12000, 'football@uk.com', 'Sport Street 15', 2147483647, 'www.footbal.com', 'Sport', '248779', 'Manchester'),
+(8, 'Color Run Festival', '2022-04-12 08:32:07', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2015/11/15/16/25/friends-1044457__340.jpg', 5000, 'run@rn.com', 'Run Street', 2147483647, 'run.rn.com', 'Sport', '1247895', 'London'),
+(9, 'Easter Music again', '2022-04-13 11:29:34', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2022/04/02/13/43/music-7107045__340.jpg', 10000, 'easter@easter.com', 'Eggstreeet 30', 87423699, 'egg-festival.com', 'Music', '12887', 'Liverpool'),
+(10, 'Romeo and Juliet', '2022-04-12 08:39:04', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum expedita non sed accusantium rem voluptate perferendis dolor doloribus maxime cumque?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, hic!', 'https://cdn.pixabay.com/photo/2013/02/26/01/10/auditorium-86197__340.jpg', 4000, 'romeoandjuliet@teather.com', 'Teater Street 20', 557456963, 'romeoandjuliet.com', 'Theather', '658741123', 'London');
+
 -- --------------------------------------------------------
 
 --
@@ -113,7 +128,7 @@ ALTER TABLE `messenger_messages`
 -- AUTO_INCREMENT dla tabeli `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT dla tabeli `messenger_messages`
